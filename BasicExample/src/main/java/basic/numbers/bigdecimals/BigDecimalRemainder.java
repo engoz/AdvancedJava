7a0ef3a302765e76200cd9162709fca752306fe3
enginoz <engoz@msn.com>
2021-09-22 13:35:57 +0300

    reminder test

--- BasicExample/src/main/java/basic/numbers/bigdecimals/BigDecimalRemainder.java
+++ BasicExample/src/main/java/basic/numbers/bigdecimals/BigDecimalRemainder.java
@@ -8,5 +8,9 @@ public class BigDecimalRemainder {
         BigDecimal b1 = new BigDecimal("4.2445");
         //Mutlak Deger
         System.out.println(b1.remainder(new BigDecimal(1)));
+
+        BigDecimal b2 = new BigDecimal("10000.12");
+        //Mutlak Deger
+        System.out.println(b2.remainder(new BigDecimal(1)));
     }
 }
