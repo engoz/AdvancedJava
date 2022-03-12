d79c35e507b8e7286c8e6e3b40928e59729f4b21
enginoz <engoz@msn.com>
2022-03-13 01:52:22 +0300

    AddNew Class

--- ConcurrentExample/src/main/java/com/eng/collections/ComputeAppSent.java
+++ ConcurrentExample/src/main/java/com/eng/collections/ComputeAppSent.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class ComputeAppSent {
     public static void main(String[] args) {
         Map<String, Integer> stringLength = new HashMap<>();
+        stringLength.put("John",3);
         Integer john = stringLength.computeIfAbsent("John", s -> s.length()+ 5);
         System.out.println(john);
         System.out.println(stringLength);
