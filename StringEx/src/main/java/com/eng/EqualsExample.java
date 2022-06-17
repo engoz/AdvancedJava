6a7b1b310a8eeb02baf4411c96870801f216f4f2
Engin OZ <engoz@msn.com>
2022-06-17 09:06:12 +0300

    Add string example

---
+++ StringEx/src/main/java/com/eng/EqualsExample.java
@@ -0,0 +1,39 @@
+package com.eng;
+
+public class EqualsExample {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = new String("hello");
+        String s3 = "hello";
+
+        //1
+        if (s1 == s2 ){
+            System.out.println("s1 == s2 equals");
+        }else {
+            System.out.println("s1 == s2 not equals");
+        }
+
+        //2
+        if (s1 == s3 ){
+            System.out.println("s1 == s3 equals");
+        }else {
+            System.out.println("s1 == s3 not equals");
+        }
+
+        //3
+        if (s1.equals(s2) ){
+            System.out.println("s1 == s2 equals");
+        }else {
+            System.out.println("s1 == s2 not equals");
+        }
+
+        //4
+        if (s1.equals(s3) ){
+            System.out.println("s1 == s3 equals");
+        }else {
+            System.out.println("s1 == s3 not equals");
+        }
+
+    }
+
+}
